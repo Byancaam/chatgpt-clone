7be33984863c8405c70287409ec9d7034913bab8
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-04-04 10:35:50 -0300

    feat: importação da floha de estilo no componentes da pasta app

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import "../styles/globals.css";
+
+import React from 'react';
+
+interface HomepageProps {
+  
+}
+
+const HomePage: React.FC<HomepageProps> = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-white">
+      <h1 className="text-5xl font-bold mb-20 ">ChatGPT </h1>
+    </div>
+  );
+};
+
+export default HomePage;
