fccc07bc49daf226e7ae4e5fe4f2ac8e8e1ca8f4
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-04-04 23:23:30 -0300

    feat: Criando a estrutura base da página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,31 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
+import { SunIcon } from '@heroicons/react/24/solid';
 
 import React from 'react';
 
-interface HomepageProps {
-  
-}
+interface HomepageProps {}
 
 const HomePage: React.FC<HomepageProps> = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-white">
-      <h1 className="text-5xl font-bold mb-20 ">ChatGPT </h1>
+    <div className='flex flex-col items-center justify-center h-screen text-white'>
+      <h1 className='text-5xl font-bold mb-20 '>ChatGPT </h1>
+
+      <div>
+        <div>
+          <div className='flex flex-col items-center justify-center mb-5'>
+            <SunIcon className='h-8 w-8   '/>
+            <h2>Examples</h2>
+          </div>
+
+          <div className='space-y-2'>
+            <p className='infoText'>Explain something to me</p>
+            <p className='infoText'>
+              What is the diference between a dog and a cat?
+            </p>
+            <p className='infoText'>What is the color of the sun?</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
