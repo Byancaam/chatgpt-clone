b2067b0aeb5bf43130cc731b04bb012d493924be
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-04-10 12:37:57 -0300

    feat: Atualizando os icones e informacoes da pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.css';
-import { SunIcon } from '@heroicons/react/24/solid';
+import { SunIcon,BoltIcon, ExclamationTriangleIcon } from '@heroicons/react/24/outline';
 
 import React from 'react';
 
@@ -10,7 +10,7 @@ const HomePage: React.FC<HomepageProps> = () => {
     <div className='flex flex-col items-center justify-center h-screen text-white'>
       <h1 className='text-5xl font-bold mb-20 '>ChatGPT </h1>
 
-      <div>
+      <div className='flex space-x-2 text-center'>
         <div>
           <div className='flex flex-col items-center justify-center mb-5'>
             <SunIcon className='h-8 w-8   '/>
@@ -18,11 +18,41 @@ const HomePage: React.FC<HomepageProps> = () => {
           </div>
 
           <div className='space-y-2'>
-            <p className='infoText'>Explain something to me</p>
-            <p className='infoText'>
-              What is the diference between a dog and a cat?
+            <p className="infoText">{`"Explain quantum computing in simple terms"`}</p>
+            <p className="infoText">
+              {`"Got any creative ideas for a 10 year old's birthday?"`}
             </p>
-            <p className='infoText'>What is the color of the sun?</p>
+            <p className="infoText">{`"How do I make an HTTP request in Javascript"`}</p>
+          </div>
+        </div>
+
+        <div>
+          <div className='flex flex-col items-center justify-center mb-5'>
+            <BoltIcon className='h-8 w-8   '/>
+            <h2>Capabilities</h2>
+          </div>
+
+          <div className='space-y-2'>
+            <p className="infoText">{`"Remembers what user said earlier in the conversation"`}</p>
+            <p className="infoText">
+              {`"Allows user to provide follow-up corrections"`}
+            </p>
+            <p className="infoText">{`"Trained to decline inappropriate requests"`}</p>
+          </div>
+        </div>
+
+        <div>
+          <div className='flex flex-col items-center justify-center mb-5'>
+            <ExclamationTriangleIcon className='h-8 w-8   '/>
+            <h2>Limitations</h2>
+          </div>
+
+          <div className='space-y-2'>
+            <p className="infoText">{`"May occasionally generate incorrect information"`}</p>
+            <p className="infoText">
+              {`"May occasionally produce harmful instructions or biased content"`}
+            </p>
+            <p className="infoText">{`"Limited knowledge of world and events after 2021"`}</p>
           </div>
         </div>
       </div>
