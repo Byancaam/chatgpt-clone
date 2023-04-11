5cadad5272c92ba1af0c3ba45120e3d4cb8d857f
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-04-11 17:01:04 -0300

    feat: Implementando o signIn na sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,15 @@
+'use client';
+
+import { useSession, signOut } from 'next-auth/react';
 import NewChat from './NewChat';
 
-function SideBar () {
+function SideBar() {
+  const { data: session } = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
-      <div className="flex-1"> 
+      <div className="flex-1">
         <div>
-          
+
           {/**New Chat */}
           <NewChat />
 
@@ -14,10 +18,20 @@ function SideBar () {
           </div>
 
           {/**Map through the CharTows */}
-        
+
         </div>
       </div>
+
+      {
+        session &&
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          onClick={() => signOut()}
+          className="w-12 h-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          src={session.user?.image!}
+          alt='Profile picture' />
+      }
     </div>
-  )
+  );
 }
 export default SideBar;
