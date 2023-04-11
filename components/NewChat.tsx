b412af105094ad6be0006c315410d4828d1bd5b7
Bianca Amarante Magalhães <bianca.amarante@venturus.org.br>
2023-04-11 17:02:39 -0300

    feat: Crianco um novo chat no firebase de acordo com a sessao do usuario

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,30 @@
+'use client';
+
+import { db } from '@/firebase';
 import { PlusIcon } from '@heroicons/react/24/solid';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 function NewChat() {
-  return(
-    <div className="border-gray-700 border chatRow">
-      <PlusIcon className='h-4 w-4'/>
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createNewChat = async () => {
+    const doc = await addDoc(
+      collection(db, "users", session?.user?.email!, "chats"), {
+        userId: session?.user?.email!,
+        creadedAt: serverTimestamp()
+      }
+    );
+
+    router.push(`/chat/${doc.id}`)
+
+  };
+
+  return (
+    <div onClick={createNewChat} className="border-gray-700 border chatRow">
+      <PlusIcon className='h-4 w-4' />
       <h1>New Chat</h1>
       <p></p>
     </div>
